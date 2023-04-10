e557b83b10d739d32c5bacb26cdea140228b5b85
Nishant Minerva <nishantcafe@gmail.com>
2023-04-10 13:12:03 +0530

    Fixing Error

--- NoQueue/Model/PageIntro.swift
+++ NoQueue/Model/PageIntro.swift
@@ -16,7 +16,7 @@ struct PageIntro: Identifiable, Hashable {
 }
 
 var pageIntros: [PageIntro] = [
-    .init(introAssetImage: "one", title: "Connect With\nCreators Easily", subTitle: "Thank you for choosing us, we can save your lovely time"),
+    .init(introAssetImage: "one", title: "Connect With\nCreators Easily", subTitle: "Thanking you for choosing us, we can save your lovely time"),
     .init(introAssetImage: "two", title: "Get Inspiration\nFrom Creator", subTitle: "Find your favourite creator and get inspired by them"),
     .init(introAssetImage: "three", title: "Let's \nGet Started", subTitle: "To register for an account, kindly enter your details", displaysAction: true)
 ]
