ee784504f39b0b69b9cdab3eb58565b06f0768c0
Nishant Minerva <nishantcafe@gmail.com>
2023-04-10 12:56:21 +0530

    Finishing the Animation

--- NoQueue/View/CustomIndicatorView.swift
+++ NoQueue/View/CustomIndicatorView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct CustomIndicatorView: View {
 //    View Properties
-    var totalPage: Int
+    var totalPages: Int
     var currentPage: Int
     var activeTint: Color = .black
     var inActiveTint: Color = .gray.opacity(0.5)
     var body: some View {
         HStack(spacing: 0) {
-            ForEach(0..<totalPage, id: \.self) {
+            ForEach(0..<totalPages, id: \.self) {
                 Circle()
                     .fill(currentPage == $0 ? activeTint: inActiveTint)
                     .frame(width: 4,height: 4)
--- NoQueue/View/Home.swift
+++ NoQueue/View/Home.swift
@@ -5,21 +5,23 @@
 //  Created by Nishant Minerva on 31/03/23.
 //
 
+
 import SwiftUI
 
 struct Home: View {
-    //        View Properties
+    /// View Properties
     @State private var activeIntro: PageIntro = pageIntros[0]
     @State private var emailID: String = ""
     @State private var password: String = ""
+    @State private var keyboardHeitght: CGFloat = 0
     var body: some View {
-        GeometryReader{
+        GeometryReader {
             let size = $0.size
             
             IntroView(intro: $activeIntro, size: size) {
-//                User Login/Signup View
+                /// User Login/Signup View
                 VStack(spacing: 10) {
-//                    Custom TextField
+                    /// Custom TextField
                     CustomTextField(text: $emailID, hint: "Email Address", leadingIcon: Image(systemName: "envelope"))
                     CustomTextField(text: $password, hint: "Password", leadingIcon: Image(systemName: "lock"), isPassword: true)
                     
@@ -28,24 +30,35 @@ struct Home: View {
                     Button {
                         
                     } label: {
-                        Text("Continue")
+                        Text("Continus")
                             .fontWeight(.semibold)
                             .foregroundColor(.white)
                             .padding(.vertical, 15)
                             .frame(maxWidth: .infinity)
-                            .background{
+                            .background {
                                 Capsule()
                                     .fill(.black)
                             }
                     }
                 }
-                padding(.top, 25)
             }
         }
         .padding(15)
+        /// Manual Keyboard Push
+        .offset(y: -keyboardHeitght)
+        /// Disabling Native Keyboard Push
+        .ignoresSafeArea(.keyboard, edges: .all)
+        .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)) { output in
+            if let info = output.userInfo, let height = (info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height {
+                keyboardHeitght = height
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardDidHideNotification)) { _ in
+            keyboardHeitght = 0
+        }
+        .animation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0), value: keyboardHeitght)
     }
 }
-
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
@@ -53,7 +66,7 @@ struct Home_Previews: PreviewProvider {
     }
 }
 
-//Intro View
+/// Intro View
 struct IntroView<ActionView: View>: View {
     @Binding var intro: PageIntro
     var size: CGSize
@@ -65,13 +78,12 @@ struct IntroView<ActionView: View>: View {
         self.actionView = actionView()
     }
     
-//    Animation Properties
+    /// Animation Properties
     @State private var showView: Bool = false
     @State private var hideWholeView: Bool = false
-    
     var body: some View {
         VStack {
-//             Image View
+            /// Image View
             GeometryReader {
                 let size = $0.size
                 
@@ -81,13 +93,14 @@ struct IntroView<ActionView: View>: View {
                     .padding(15)
                     .frame(width: size.width, height: size.height)
             }
-//            Moving Up
+            /// Moving Up
             .offset(y: showView ? 0 : -size.height / 2)
             .opacity(showView ? 1 : 0)
             
-//            Title & Action's
+            /// Title & Action's
             VStack(alignment: .leading, spacing: 10) {
                 Spacer(minLength: 0)
+                
                 Text(intro.title)
                     .font(.system(size: 40))
                     .fontWeight(.black)
@@ -95,16 +108,17 @@ struct IntroView<ActionView: View>: View {
                 Text(intro.subTitle)
                     .font(.caption)
                     .foregroundColor(.gray)
-                    .padding(.top, 15)
                 
                 if !intro.displaysAction {
-                    Group{
+                    Group {
                         Spacer(minLength: 25)
-//                        Custom Indicator View
-                        CustomIndicatorView(totalPage: filteredPages.count, currentPage: filteredPages.firstIndex(of: intro) ?? 0)
+                        
+                        /// Custom Indicator View
+                        CustomIndicatorView(totalPages: filteredPages.count, currentPage: filteredPages.firstIndex(of: intro) ?? 0)
                             .frame(maxWidth: .infinity)
                         
                         Spacer(minLength: 10)
+                        
                         Button {
                             changeIntro()
                         } label: {
@@ -117,27 +131,29 @@ struct IntroView<ActionView: View>: View {
                                     Capsule()
                                         .fill(.black)
                                 }
-                                .frame(maxWidth : .infinity)
                         }
+                        .frame(maxWidth: .infinity)
                     }
                 } else {
-//                    Action View
-                    
+                    actionView
+                        .offset(y: showView ? 0 : size.height / 2)
+                        .opacity(showView ? 1 : 0)
                 }
-                
             }
             .frame(maxWidth: .infinity, alignment: .leading)
-//            Moving Down
-            .offset(y: showView ? 0 : -size.height / 2)
+            /// Moving Down
+            .offset(y: showView ? 0 : size.height / 2)
             .opacity(showView ? 1 : 0)
         }
-//        BackButton
+        .offset(y: hideWholeView ? size.height / 2 : 0)
+        .opacity(hideWholeView ? 0 : 1)
+        /// Back Button
         .overlay(alignment: .topLeading) {
-//            Hiding it for very First Page, Since there is no previos page present
+            /// Hiding it for Very First Page, Since there is no previous page present
             if intro != pageIntros.first {
                 Button {
                     changeIntro(true)
-                } label: {
+                } label : {
                     Image(systemName: "chevron.left")
                         .font(.title2)
                         .fontWeight(.semibold)
@@ -145,6 +161,11 @@ struct IntroView<ActionView: View>: View {
                         .contentShape(Rectangle())
                 }
                 .padding(10)
+                /// Animating Back Button
+                /// Comes From Top When Active
+                .offset(y: showView ? 0 : -200)
+                /// Hides by Going back to Top When In Active
+                .offset(y: hideWholeView ? -200 : 0)
             }
         }
         .onAppear {
@@ -153,17 +174,29 @@ struct IntroView<ActionView: View>: View {
             }
         }
     }
-//    Updating Page Intros's
+    
+    /// Updating Page Intro's
     func changeIntro(_ isPrevious: Bool = false) {
-//        if let index = pageIntros.firstIndex(of: intro), index != pageIntros.count - 1 {
-        if let index = pageIntros.firstIndex(of: intro), (isPrevious ? index != 0 : index != pageIntros.count - 1) {
-//            intro = pageIntros[index + 1]
-            intro = isPrevious ? pageIntros[index - 1] : pageIntros[index + 1]
-        } else {
-//            intro = pageIntros[pageIntros.count - 1]
-            intro = isPrevious ? pageIntros[0] : pageIntros[pageIntros.count  - 1]
+        withAnimation(.spring(response: 0.8, dampingFraction: 0.8, blendDuration: 0)) {
+            hideWholeView = true
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            if let index = pageIntros.firstIndex(of: intro), (isPrevious ? index != 0 : index != pageIntros.count - 1) {
+                intro = isPrevious ? pageIntros[index - 1] : pageIntros[index + 1]
+            } else {
+                intro = isPrevious ? pageIntros[0] : pageIntros[pageIntros.count - 1]
+            }
+            /// Re-Animating as Split Page
+            hideWholeView = false
+            showView = false
+            
+            withAnimation(.spring(response: 0.8, dampingFraction: 0.8, blendDuration: 0)) {
+                showView = true
+            }
         }
     }
+    
     var filteredPages: [PageIntro] {
         return pageIntros.filter { !$0.displaysAction }
     }
